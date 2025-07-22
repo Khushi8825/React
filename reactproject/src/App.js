6af426c645d60de284613c29c3cc630c2aa7b651
khushi8825 <seemasingh882533@gmail.com>
2025-07-22 23:35:47 +0530

    Added reactproject files

---
+++ reactproject/src/App.js
@@ -0,0 +1,10 @@
+
+//in this also we have to keep our function name start with capital letter
+import Khushi from "./khushi";
+function App() {
+  return (
+    <Khushi/>
+  );
+}
+
+export default App;
---
+++ reactproject/src/khushi.js
@@ -0,0 +1,11 @@
+//creating own component
+
+function Khushi(){
+    return(
+        <>
+        <h1>Welcome to my project</h1>
+        <h1>React project using CRA</h1>
+        </>
+    )
+}
+export default Khushi;
